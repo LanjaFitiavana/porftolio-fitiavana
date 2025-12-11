5e66c980cb19e0aeebe1c1682aff519d856f85f3
LanjaFitiavana <010rabeza4gmail.com>
2025-12-11 13:52:52 +0100

    maj

--- messageemail/views.py
+++ messageemail/views.py
@@ -1,46 +1,46 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views.generic import FormView
 from .forms import ContactForm
 from django.urls import reverse_lazy
 from django.core.mail import send_mail
-from django.conf  import settings
+from django.conf import settings
 from django.contrib import messages
 
 
 class Home(FormView):
     template_name = 'messageemail/home.html'
     form_class = ContactForm
-    success_url = reverse_lazy ('home')
+    success_url = reverse_lazy('home')
 
-    def form_valid(self,form):
+    def form_valid(self, form):
         message_instance = form.save()
+
         nom = message_instance.nom
         email_exp = message_instance.email
         contenu = message_instance.contenu
-        
-        contenu_email = f"""
-        Nom:{nom}
-        Email:{email_exp}
-        Message:{contenu}
-        """
+
+        contenu_email = (
+            f"Nom : {nom}\n"
+            f"Email : {email_exp}\n"
+            f"Message :\n{contenu}"
+        )
+
         try:
             send_mail(
-                subject=f"nouveau message de :{nom}",
+                subject=f"Nouveau message de : {nom}",
                 message=contenu_email,
                 from_email=settings.DEFAULT_FROM_EMAIL,
                 recipient_list=[settings.MON_EMAIL_DE_RECEPTION],
-                fail_silently=False,
-                
-                
+                fail_silently=False
             )
-            print(f"Bien envoyer")
-            messages.success(self.request,"Votre message a été envoyé avec succès !")
+            messages.success(self.request, "Votre message a été envoyé avec succès !")
         except Exception as e:
-            print(f"erreur d'envoie:{e}")
-            messages.error(self.request,"Erreur de l'envoie de l'email.Veuillez resayer   s'il vous plait.")
+            print(f"Erreur d'envoi : {e}")
+            messages.error(self.request, "Erreur lors de l'envoi de l'email. Veuillez réessayer.")
 
         return super().form_valid(form)
-     #a propos
+
+    
 
 
     
