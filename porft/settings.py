575f33e3d43e8285933a8f8a354144e178dd6471
LanjaFitiavana <010rabeza4gmail.com>
2025-12-11 14:30:47 +0100

    q

--- porft/settings.py
+++ porft/settings.py
@@ -32,7 +32,6 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = [
-    'porftolio.fitiavana.onrender.com'
     '.onrender.com',
     'localhost',
     '127.0.0.1',
