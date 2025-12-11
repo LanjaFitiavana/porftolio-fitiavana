d63fa14526e96cb54bfe17241138282f63e44248
LanjaFitiavana <010rabeza4gmail.com>
2025-12-11 11:20:32 +0100

    maj

--- messageemail/models.py
+++ messageemail/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class MessageEmail(models.Model):
-    nom = models.CharField()
+    nom = models.CharField(max_length=100)
     contenu = models.TextField()
     email = models.EmailField()
     date_sms = models.DateTimeField(auto_now_add=True)
