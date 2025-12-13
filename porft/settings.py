97102974ad9562e3ea89c4e1de825e81b3c5dfb2
LanjaFitiavana <010rabeza4gmail.com>
2025-12-13 14:26:34 +0100

    derniere maj

--- porft/settings.py
+++ porft/settings.py
@@ -81,10 +81,18 @@ WSGI_APPLICATION = 'porft.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': env.db(),
-}
 
+
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': env('DATABASE_NAME'),
+         'USER': env('DATABASE_USER'),
+         'PASSWORD': env('DATABASE_PASSWORD'),
+         'HOST': env('DATABASE_HOST'),
+         'PORT': env('DATABASE_PORT'),
+     }
+}
 
 
 
