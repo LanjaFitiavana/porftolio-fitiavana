73987c17d994b3a4b3b8e046dbe6d5a1dda9ecc9
LanjaFitiavana <010rabeza4gmail.com>
2025-12-16 00:08:05 +0100

    n

--- messageemail/views.py
+++ messageemail/views.py
@@ -1,16 +1,13 @@
-
-from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 from django.views.generic import FormView
-from .forms import ContactForm
 from django.urls import reverse_lazy
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib import messages
 
 
-class Home(FormView):
+class Home(TemplateView):
     template_name = 'messageemail/home.html'
-    form_class = ContactForm
     success_url = reverse_lazy('home')
 
     
